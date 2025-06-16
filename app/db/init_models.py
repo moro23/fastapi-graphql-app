e018db6ab2fc8ca5bbf7aa88f1d973f030a91f29
moro23 <billhab66@gmail.com>
2025-06-16 11:51:40 +0000

    updated the init_models file.

--- app/db/init_models.py
+++ app/db/init_models.py
@@ -1,7 +1,16 @@
-from domains.auth.models.refresh_token import APIBase
+
 from domains.auth.models.users import APIBase
 from db.session import engine
 
+
+## lets import all our models here so that they are registered with APIBase
+from domains.auth.models.users import User
+from domains.auth.models.refresh_token import RefreshToken
+
+
+from domains.faq.models import FAQCategory, FAQItem
+from domains.audit.models import AuditLog
+from domains.feedback.models import Feedback
 
 def create_tables():
     APIBase.metadata.create_all(bind=engine)
