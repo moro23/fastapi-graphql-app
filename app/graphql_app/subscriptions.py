97e36159eff370a52024b9aa9d142b09453647ea
moro <moroa@aiti-kace.com.gh>
2025-06-23 11:14:10 +0000

    Add subscription for counting with Strawberry GraphQL

---
+++ app/graphql_app/subscriptions.py
@@ -0,0 +1,15 @@
+import stawberry 
+import asyncio 
+import typing import AsyncGenerator, Optional
+from strawberry.types import Info
+from .types import FAQItemType, FeedbackType 
+
+
+@strawberry.type 
+class Subscription:
+    @strawberry.subscription 
+    async def count(self, target: int = 10) -> AsyncGenerator[int, None]:
+        """A simple subscription that counts up to a target number."""
+        for i in range(target):
+            yield i
+            await asyncio.sleep(0.5)
