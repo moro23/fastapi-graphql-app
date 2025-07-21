c817da81ead7ba03bd0796a2aaa29ad73da5a5e7
moro <moroa@aiti-kace.com.gh>
2025-07-21 16:15:08 +0000

    Refactor permission classes to improve user role validation and enhance code clarity

--- app/graphql_app/permissions.py
+++ app/graphql_app/permissions.py
@@ -6,28 +6,40 @@ from domains.auth.models.roles import Role as RoleModel
 from sqlalchemy.orm import Session 
 
 ## lets create helper functions to get the current user and role from the context 
-def get_current_user(info: Info) -> Optional[UserModel]:
+def get_current_user_from_context(info: Info) -> Optional[UserModel]:
     return info.context.get("current_user")
 
 class IsAuthenticated(strawberry.permission.BasePermission):
     message = "User is not authenticated."
-    def has_permission(self, source: Any, info: Info) -> bool:
-        current_user = get_current_user(info)
+    def has_permission(self, source: Any, info: Info, **kwargs) -> bool:
+        current_user = get_current_user_from_context(info)
         return current_user is not None
     
-class HasRole(strawberry.permission.BasePermission):
+class _HasRole(strawberry.permission.BasePermission):
     message = "User does not have the required role."
+
+    required_roles: List[str] = []
     
-    def __init__(self, role_name: str):
-        self.role_name = role_name
     
-    def has_permission(self, source: Any, info: Info) -> bool:
-        current_user = get_current_user(info)
+    def has_permission(self, source: Any, info: Info, **kwargs) -> bool:
+        current_user = get_current_user_from_context(info)
         if not current_user:
             return False
+        
         db: Session = info.context["db"]
         user_role = db.query(RoleModel).filter(RoleModel.id == current_user.role_id).first()
 
         if not user_role or user_role.name not in self.required_roles:
             return False
         return True 
+    
+def HasRole(roles: List[str]) -> type:
+    """
+    A factory that creates a new permission class with the required roles
+    """
+
+    return type(
+        "HasRolePermission", 
+        (_HasRole,),
+        {"required_roles": roles}
+    )
